fddf3b4eb449cead6a3f4fe8b52ba22bf8b4299c
2011Karthik <100004622+2011Karthik@users.noreply.github.com>
2023-06-19 12:45:43 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
+scoreLeftWrist = 0;
 function preload()
 {
     song1 = loadSound("music.mp3");
@@ -28,13 +29,40 @@ function modelLoaded() {
 function draw() 
 {
     image(video, 0, 0, 600, 500);
+    fill("#FF0000");
+    stroke("#FF0000");
+
+    if(scoreLeftWrist > 0.2)
+    {
+        circle(leftWristX,leftWristY,20);
+        InNumberleftWristY = Number(leftWristY);
+        remove_decimals = floor(InNumberleftWristY);
+        volume = remove_decimals/500;
+        document.getElementById("volume").innerHTML = "Volume = " + volume;
+
+        if(status == song1)
+        {
+            document.getElementById("volume = song1.volume");
+        }
+
+        if(status == song2)
+        {
+            document.getElementById("volume = song2.volume");
+        }
+
+    }
+  
 }
 
 function play()
 {
-    song.play();
-    song.setVolume(1);
-    song.rate(1);
+    song1.play();
+    song1.setVolume(1);
+    song1.rate(1);
+    song2.play();
+    song2.setVolume(1);
+    song2.rate(1);
+
 }
 
 function gotPoses(results)
@@ -42,6 +70,8 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX = " + leftWristX +" leftWristY = "+leftWristY);
